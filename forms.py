197b5f035c0c13a334d2881b5127dea48ad75779
Peter Fleming <pefleming87@gmail.com>
2020-12-02 16:16:02 +0100

    Auto-complete ugly but running

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextField
 
 
 class DrugQueryForm(FlaskForm):
-    drugname = StringField('drugname')
+    drugname = StringField('drugname', id="drug_name_autocomplete")
     submit = SubmitField('request')
